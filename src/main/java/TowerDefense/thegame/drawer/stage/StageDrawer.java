40ab79ac46c8032efd1565877443e0a09603a2e5
namlh201 <43928339+namlh201@users.noreply.github.com>
2019-11-07 23:43:52 +0700

    Optimised performance

--- src/main/java/TowerDefense/thegame/drawer/stage/StageDrawer.java
+++ src/main/java/TowerDefense/thegame/drawer/stage/StageDrawer.java
@@ -10,10 +10,13 @@ import java.util.Scanner;
 
 public class StageDrawer {
     private GraphicsContext graphicsContext;
+    private Image image;
     private String[][] layout = new String[(int) Config.TILE_VERTICAL][];
 
     public StageDrawer(GraphicsContext graphicsContext, String filePath) throws IOException {
         this.graphicsContext = graphicsContext;
+
+        this.image = new Image(new FileInputStream("src/main/java/TowerDefense/thegame/drawer/stage/TestStageImage.png"));
 
         Scanner scan = new Scanner(new FileInputStream(filePath));
         for (int i = 0; i < Config.TILE_VERTICAL; ++i) {
@@ -22,19 +25,18 @@ public class StageDrawer {
         }
     }
 
-    public void draw() throws IOException {
-        String path = "src/main/java/TowerDefense/thegame/drawer/stage/";
+    public void draw() {
+//        String path = "src/main/java/TowerDefense/thegame/drawer/stage/";
 
-        graphicsContext.drawImage(new Image(new FileInputStream(path + "AnotherTestStageBG.png")),
-                0, 0, Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
+        graphicsContext.drawImage(image, 0, 0, Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
 
-        for (int i = 0; i < Config.TILE_VERTICAL; ++i) {
-            for (int j = 0; j < Config.TILE_HORIZONTAL; ++j) {
-                if (!layout[i][j].equals("000")) {
-                    graphicsContext.drawImage(new Image(new FileInputStream(path + layout[i][j] + ".png")),
-                            j * Config.TILE_SIZE, i * Config.TILE_SIZE, Config.TILE_SIZE, Config.TILE_SIZE);
-                }
-            }
-        }
+//        for (int i = 0; i < Config.TILE_VERTICAL; ++i) {
+//            for (int j = 0; j < Config.TILE_HORIZONTAL; ++j) {
+//                if (!layout[i][j].equals("000")) {
+//                    graphicsContext.drawImage(new Image(new FileInputStream(path + layout[i][j] + ".png")),
+//                            j * Config.TILE_SIZE, i * Config.TILE_SIZE, Config.TILE_SIZE, Config.TILE_SIZE);
+//                }
+//            }
+//        }
     }
 }
