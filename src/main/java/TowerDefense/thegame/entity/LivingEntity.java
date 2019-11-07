aa86e2c3b48318c3f029db99604ebb48f236dd98
Hoang Vu Duy Anh <vuduyanhhoang33@gmail.com>
2019-11-07 14:05:21 +0700

    Update LivingEntity.java
    add doEffect

--- src/main/java/TowerDefense/thegame/entity/LivingEntity.java
+++ src/main/java/TowerDefense/thegame/entity/LivingEntity.java
@@ -4,5 +4,6 @@ import TowerDefense.thegame.entity.effect.AbstractEffect;
 
 public interface LivingEntity extends DestroyableEntity {
 	long getHealth();
+	void doEffect(long value);
 	void getEffected(AbstractEffect other);
 }
