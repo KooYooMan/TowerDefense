4d03bb4868dcf95c625d1578e952e7484c7de997
Hoang Vu Duy Anh <vuduyanhhoang33@gmail.com>
2019-11-07 10:47:03 +0700

    Add tower

---
+++ src/main/java/TowerDefense/thegame/entity/tower/NormalTower.java
@@ -0,0 +1,12 @@
+package TowerDefense.thegame.entity.tower;
+
+import TowerDefense.thegame.Config;
+import TowerDefense.thegame.entity.gun.NormalGun;
+import TowerDefense.thegame.entity.bullet.NormalBullet;
+
+public class NormalTower extends AbstractTower {
+    public NormalTower(double posX, double posY) {
+        super(posX, posY, Config.NORMAL_TOWER_WIDTH, Config.NORMAL_TOWER_HEIGHT, Config.NORMAL_TOWER_RANGE, Config.NORMAL_TOWER_SPEED,
+                new NormalGun(posX, posY), NormalBullet.class);
+    }
+}
